31a72b96da09663658a821d5e26a8519ab978426
apple <apple@appletekiMacBook-Air-2.local>
2015-01-22 13:39:50 +0800

    Initial Commit

---
+++ HelloWorld 2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld 2
+//
+//  Created by apple on 15/1/22.
+//  Copyright (c) 2015年 apple.yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
